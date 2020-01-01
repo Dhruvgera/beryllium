d4567da4dcc9796d4c1cb028a0c86cb314112426
dhruv <dhruvgera61@gmail.com>
2020-01-01 12:15:39 +0000

    Add Gcam

--- sdm845.mk
+++ sdm845.mk
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap \
+    Gcam \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
